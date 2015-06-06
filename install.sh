08d34755b4ceadc44362f831457a61a9e4391872
Miroslav Safr <Miroslav.Safr@gmail.com>
2015-06-07 00:21:12 +0300

    Changes: appver-installer update
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- install.sh
+++ install.sh
@@ -1,30 +1,10 @@
 #/bin/sh
 #easier way how to handle version formats http://safrm.net/projects/appver/
 #author:  Miroslav Safr <miroslav.safr@gmail.com>
-BINDIR=/usr/bin/
-MANDIR=/usr/share/man
-
-#root check
-USERID=`id -u`
-[ $USERID -eq "0" ] || {
-    echo "I cannot continue, you should be root or run it with sudo!"
-    exit 0
-}
-
-#test
-sh -n ./appver
-if  [ $? != 0 ]; then
-	echo "syntax error in appver, exiting.."
-	exit 1
-fi
-
 . ./appver
-install -m 0777 -v ./appver  $BINDIR
-sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/appver && rm -f $BINDIR/appver.bkp
-sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/appver && rm -f $BINDIR/appver.bkp
+. ./appver-installer
 
-MANPAGES=`find ./doc/manpages -type f`
-install -d -m 755 $MANDIR/man1
-install -m 644 $MANPAGES $MANDIR/man1
-
+$INSTALL_755 ./appver $BINDIR
+$INSTALL_755 ./appver-installer $BINDIR
+update_version_and_date() $BINDIR/appver
 
